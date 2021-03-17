36b0dcbd75b49db98e116958917f67dd6028a9f0
Alina <alinkartemenko@mail.ru>
2021-03-17 19:05:05 +0300

    lesson 3

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
-const question1 = {
-  name: ''
+// let num = '0';
+// let score;
+//
+// const question1 = {
+//   name: ''
+// }
+// const question2 = {
+//   name: ''
+// }
+// const question3 = {
+//   name: ''
+// }
+// const question4 = {
+//   name: ''
+// }
+
+const a = -3;
+if (a === 0) {
+  console.log('верно');
+} else {
+  console.log('не верно')
 }
-const question2 = {
-  name: ''
-}
-const question3 = {
-  name: ''
-}
-const question4 = {
-  name: ''
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 == 0) {
+    console.log('число - четное');
+  } else {
+    console.log('число - нечетное')
+  }
 }
