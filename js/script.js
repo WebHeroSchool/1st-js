1d79ad9b404e00dc371e0e349dfa434680155f4a
Alina <alinkartemenko@mail.ru>
2021-03-25 17:31:11 +0300

    lesson 5 functions

--- js/script.js
+++ js/script.js
@@ -1,30 +1,80 @@
-// let num = '0';
-// let score;
-//
-// const question1 = {
-//   name: ''
-// }
-// const question2 = {
-//   name: ''
-// }
-// const question3 = {
-//   name: ''
-// }
-// const question4 = {
-//   name: ''
-// }
+let num = '0';
+let score;
 
-const a = -3;
-if (a === 0) {
-  console.log('верно');
-} else {
-  console.log('не верно')
-}
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 2 == 0) {
-    console.log('число - четное');
-  } else {
-    console.log('число - нечетное')
+let questionArray = [{
+  question: 'q1',
+  correctAnswer: 'a'
+}, {
+  question: 'q2',
+  correctAnswer: 'b'
+}, {
+  question: 'q3',
+  correctAnswer: 'c'
+}, {
+  question: 'q4',
+  correctAnswer: 'c'
+}]
+
+
+
+let answerArray = [{
+  question: 'q1',
+  someAnswer: 'c'
+}, {
+  question: 'q2',
+  someAnswer: 'b'
+}, {
+  question: 'q3',
+  someAnswer: 'c'
+}, {
+  question: 'q4',
+  someAnswer: 'c'
+}]
+
+function findQuestion(q) {
+  for (i = 0; i < questionArray.length; i++) {
+    if (questionArray[i].question == q) {
+      console.log("question found:", questionArray[i].question, "answer: ", questionArray[i].correctAnswer)
+      return questionArray[i]
+    }
   }
+  return null
 }
+
+function check(a) {
+  for (i = 0; i < a.length; i++) {
+    q = findQuestion(a[i].question)
+    console.log(q)
+    if (q == null) {
+      return false
+    }
+    if (q.correctAnswer != a[i].someAnswer) {
+      console.log(q.correctAnswer, " != ", a[i].someAnswer)
+      return false
+    }
+  }
+  return true
+}
+
+console.log(check(answerArray))
+
+
+// const result = question.filter((item) => item.correctAnswer === 'c')
+// console.log(result);
+
+
+// const a = -3;
+// if (a === 0) {
+//   console.log('верно');
+// } else {
+//   console.log('не верно')
+// }
+//
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i, 'число - четное');
+//   } else {
+//     console.log(i, 'число - нечетное')
+//   }
+// }
